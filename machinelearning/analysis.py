30b62a8db97ea1ac5eb1e9951b154cbc4542722e
nedkia <78882341+nedkia@users.noreply.github.com>
2022-12-05 12:19:00 -0500

    Update analysis.py

--- machinelearning/analysis.py
+++ machinelearning/analysis.py
@@ -17,35 +17,39 @@ bcount = 0
 hcount = 0
 wcount = 0
 while i < size:
-    if lst[i]['Race'] == "Black" and (lst[i]['Severity of Crime'] == "Felony" or "Misdemeanor"):
+    if lst[i]['RACE'] == "Black" and (lst[i]['SEVERITY OF CRIME'] == "Felony" or "Misdemeanor"):
         ##print("found AA")
         bcount = bcount + 1
-    if lst[i]['Race'] == "White" and (lst[i]['Severity of Crime'] == "Felony" or "Misdemeanor"):
+    if lst[i]['RACE'] == "White" and (lst[i]['SEVERITY OF CRIME'] == "Felony" or "Misdemeanor"):
         ##print("found AA")
         wcount = wcount + 1
-    if lst[i]['Race'] == "Hispanic" and (lst[i]['Severity of Crime'] == "Felony" or "Misdemeanor"):
+    if lst[i]['RACE'] == "Hispanic" and (lst[i]['SEVERITY OF CRIME'] == "Felony" or "Misdemeanor"):
         ##print("found AA")
         hcount = hcount + 1
     
     i = i + 1
-print("Amount of African Americans with a severe crime:",bcount)
-print("Amount of White Americans with a sever crime:",wcount)
-print("Amount of Hispanics with a severe crime: ",hcount)
 
 freq_race = ""
 racecount = float(0)
+
+racecount1 = float(bcount/size) * 100
+freq_race1 = "Black"
+
+freq_race2 = "Hispanic"
+racecount2 = float(hcount/size) * 100
+
 if bcount >= wcount and bcount >= hcount:
     print("bcount is the greatest")
-    racecount = float(bcount/size) * 100
-    freq_race = "Black"
+    
 if wcount >= bcount and wcount >= hcount:
     print("wcount is the greatest")
-    freq_race = "Hispanic"
-    racecount = float(hcount/size) * 100
+  
 if hcount >= wcount and hcount >= bcount:
     print("hcount is the greatest")
-    racecount = float(wcount/size) * 100
-    freq_race = "White"
+    racecount3 = float(wcount/size) * 100
+    freq_race3 = "White"
 
-
+print("Amount of African Americans with a severe crime:",bcount)
+print("Amount of White Americans with a sever crime:",wcount)
+print("Amount of Hispanics with a severe crime: ",hcount)
 print("Based on our machine learning analysis "+ freq_race + " is the highest likely race to commit a severe crime with a "+str(racecount)+ " percent chance")
